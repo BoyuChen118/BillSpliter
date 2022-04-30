ebbec95d9ea0210aaeb278da4194d83d1c5baaea
BoyuChen118 <alexboyuchen@gmail.com>
2022-04-30 01:55:35 -0400

    updated url

--- BillSplitterApp/views.py
+++ BillSplitterApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+# test with current_datetime
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+
+def login(request):
+    return render(request, 'BillSplitterApp/login-form-v1/login_v1/index.html')
